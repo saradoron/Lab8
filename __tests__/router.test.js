10db61e4fe7f289a8ebca6b5105098737c36809a
saradoron <sdoron@ucsd.edu>
2021-05-21 12:54:27 -0700

    finished part 1

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -1,4 +1,33 @@
 /**
  * @jest-environment jsdom
  */
+ import { pushToHistory } from '../scripts/router.js';
+
+ describe('checking length when moving between pages', () => {
+    test('goes to settings', () => {
+        expect(pushToHistory('settings').length).toBe(2);
+    });
+    
+    test('goes to entry 2', () => {
+        expect(pushToHistory('entry', 2).length).toBe(3);
+    });
+    
+    test('goes main page', () => {
+        expect(pushToHistory('',).length).toBe(4);
+    });
+
+    
+    
+    test('settings branch', () => {
+        expect(pushToHistory('settings').state).toEqual({page: 'settings'});
+    });
+
+    test('entry branch', () => {
+        expect(pushToHistory('entry', 4).state).toEqual({page: 'entry4'});
+    });
+
+    test('default branch', () => {
+        expect(pushToHistory('').state).toEqual({});
+});
+  });
 
